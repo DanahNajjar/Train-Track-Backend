3fd814a495615f3eff028fcb2ff2975e94bc7a7f
DanahNajjar <najjar1234danah@gmail.com>
2025-04-22 13:33:08 +0300

    new

--- api/db.py
+++ api/db.py
@@ -2,12 +2,12 @@ import os
 import mysql.connector
 from dotenv import load_dotenv
 
-# Load .env (Render will automatically inject the env vars)
+# Load environment variables
 load_dotenv()
 
-# Force cloud DB settings
+# Read from environment (Render injects them automatically)
 host = os.getenv("MYSQL_HOST")
-port = int(os.getenv("MYSQL_PORT"))
+port = int(os.getenv("MYSQL_PORT", 3306))
 user = os.getenv("MYSQL_USER")
 password = os.getenv("MYSQL_PASSWORD")
 database = os.getenv("MYSQL_DATABASE")
