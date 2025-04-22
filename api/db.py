d22fda551d82d41040c577db8d170ad8253ae91b
DanahNajjar <najjar1234danah@gmail.com>
2025-04-22 13:09:06 +0300

    NEW

--- api/db.py
+++ api/db.py
@@ -2,9 +2,13 @@ import os
 import mysql.connector
 from dotenv import load_dotenv
 
+# Load environment variables from .env file
 load_dotenv()
+
+# Choose profile
 profile = os.getenv("PROFILE", "local")
 
+# Determine DB connection config
 if profile == "cloud":
     host = os.getenv("CLOUD_DB_HOST")
     port = int(os.getenv("CLOUD_DB_PORT"))
@@ -12,11 +16,11 @@ if profile == "cloud":
     password = os.getenv("CLOUD_DB_PASSWORD")
     database = os.getenv("CLOUD_DB_NAME")
 else:
-    host = os.getenv("LOCAL_DB_HOST")
-    port = int(os.getenv("LOCAL_DB_PORT"))
-    user = os.getenv("LOCAL_DB_USER")
-    password = os.getenv("LOCAL_DB_PASSWORD")
-    database = os.getenv("LOCAL_DB_NAME")
+    host = os.getenv("LOCAL_DB_HOST", "localhost")
+    port = int(os.getenv("LOCAL_DB_PORT", 3306))
+    user = os.getenv("LOCAL_DB_USER", "root")
+    password = os.getenv("LOCAL_DB_PASSWORD", "")
+    database = os.getenv("LOCAL_DB_NAME", "expert_system")
 
 def get_db_connection():
     return mysql.connector.connect(
