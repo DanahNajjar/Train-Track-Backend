856bb8ac6257f4556fef1a06f6398018ffe434c0
DanahNajjar <najjar1234danah@gmail.com>
2025-04-22 13:52:18 +0300

    new

--- api/db.py
+++ api/db.py
@@ -2,15 +2,17 @@ import os
 import mysql.connector
 from dotenv import load_dotenv
 
-# Only for local testing
+# Load env vars (locally only — ignored in deployment)
 load_dotenv()
 
+# Read environment variables
 host = os.getenv("MYSQL_HOST")
 port = int(os.getenv("MYSQL_PORT"))
 user = os.getenv("MYSQL_USER")
 password = os.getenv("MYSQL_PASSWORD")
 database = os.getenv("MYSQL_DATABASE")
 
+# Connection function
 def get_db_connection():
     return mysql.connector.connect(
         host=host,
