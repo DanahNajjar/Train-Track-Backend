69515c409fa7ea9d1fed4e67f8ee38e4324e4a18
DanahNajjar <najjar1234danah@gmail.com>
2025-05-22 19:18:45 +0300

    new

--- api/recommendation.py
+++ api/recommendation.py
@@ -521,7 +521,7 @@ def user_input_summary():
             cursor.close()
             connection.close()
 
-@recommendation_routes.route('/recommendations/fallback-prerequisites', methods=['POST'])
+@recommendation_routes.route('/fallback-prerequisites', methods=['POST'])
 def get_fallback_prerequisites():
     try:
         data = request.get_json()
